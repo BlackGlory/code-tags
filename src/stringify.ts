597e2636a0806c9d1d887c71d6531a5e94d12e40
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-08-12 17:24:38 +0800

    refactor: add @blackglory/types

--- src/stringify.ts
+++ src/stringify.ts
@@ -1,32 +1,18 @@
+import { isObject, isFunction, isString } from '@blackglory/types'
+
 export function stringify(val: unknown): string {
   if (isString(val)) return stringifyString(val)
   if (isFunction(val)) return stringifyFunction(val)
-  if (isArray(val)) return stringifyArray(val)
+  if (Array.isArray(val)) return stringifyArray(val)
   if (isObject(val)) return stringifyObject(val)
   return `${val}`
-}
-
-function isArray<T>(val: unknown): val is T[] {
-  return Array.isArray(val)
-}
-
-function isObject(val: unknown): val is object {
-  return typeof val === 'object'
-}
-
-function isFunction(val: unknown): val is (...args: any) => any {
-  return typeof val === 'function'
-}
-
-function isString(val: unknown): val is string {
-  return typeof val === 'string'
 }
 
 function stringifyFunction(val: (...args: any) => any): string {
   return val.toString()
 }
 
-function stringifyString(val: string): string {
+function stringifyString(val: string | String): string {
   return JSON.stringify(val)
 }
 
